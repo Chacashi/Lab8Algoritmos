49da1eb3324ab8ef4769b5b23025bb59e35e9186
Chacashi <eljohndelapuna@gmail.com>
2024-10-20 22:17:18 -0500

    Graph with error head null

--- Assets/Scripts/GraphMapController.cs
+++ Assets/Scripts/GraphMapController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GraphMapController : MonoBehaviour
 {
+    
     [SerializeField] TextAsset GraphMap;
+    [SerializeField] TextAsset ConnectionsMap;
+    string[] arrayNodeConnectionsRows;
+    string[] arrayNodeConnectionsColums;
      string[] arrayNodeRows;
      string[] arrayNodeColumns;
     [SerializeField] GameObject NodePrefab;
+   DoubleLinkList<NodeControll> ListNodes = new DoubleLinkList<NodeControll>();
+    [SerializeField] PlayerController player;
 
 
     private void Start()
     {
+        SetInitialNode();
         OnDrawGraph();
+        ConnectNodes();
     }
     void OnDrawGraph()
     {
@@ -21,9 +29,30 @@ public class GraphMapController : MonoBehaviour
         for (int i = 0; i < arrayNodeRows.Length; i++)
         {
             arrayNodeColumns = arrayNodeRows[i].Split(";");
-             Instantiate(NodePrefab, new Vector2(float.Parse(arrayNodeColumns[0]), 
+            currentNode =  Instantiate(NodePrefab, new Vector2(float.Parse(arrayNodeColumns[0]), 
                 float.Parse(arrayNodeColumns[1])), transform.rotation);
+            currentNode.name = "NODE" + i.ToString();
+            ListNodes.InsertAtEnd(currentNode.GetComponent<NodeControll>());
         }
+       
     }
 
+    void ConnectNodes()
+    {
+        
+        arrayNodeConnectionsRows = ConnectionsMap.text.Split("\n");
+        for(int i = 0; i < ListNodes.count; i++)
+        {
+            arrayNodeConnectionsColums = arrayNodeConnectionsRows[i].Split(";");
+            for(int j = 0; j < arrayNodeConnectionsColums.Length; j++)
+            {
+                ListNodes.GetValueAtPosition(i).AddAdjacentNode(ListNodes.GetValueAtPosition(int.Parse(arrayNodeConnectionsColums[j])));
+            }
+        }
+    }
+    void SetInitialNode()
+    {
+        int position = Random.Range(0, ListNodes.count);
+        player.SetNewPosition(ListNodes.GetValueAtPosition(position).gameObject.transform.position);
+    }
 }
--- Assets/Scripts/NodeControll.cs
+++ Assets/Scripts/NodeControll.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 
-public class NodeControll : MonoBehaviour
+public class NodeControll:MonoBehaviour
 {
-    
+    public DoubleLinkList<NodeControll> listAdjacentsNodes = new DoubleLinkList<NodeControll>();
+
+    public void AddAdjacentNode (NodeControll node)
+    {
+        listAdjacentsNodes.InsertAtEnd (node);
+    }
+
+    public NodeControll GetAdjacentNode()
+    {
+        return listAdjacentsNodes.GetValueAtPosition(Random.Range(0, listAdjacentsNodes.count));
+    }
 }
